3f0112bf2eef04bd5b47257155f6472cf3b4b5c8
fsjavier <javier.fersal@gmail.com>
2024-11-06 14:39:42 +0100

    feat: Add helper functions to get calendar days

--- src/utils/DateHelper.ts
+++ src/utils/DateHelper.ts
@@ -1,10 +1,40 @@
-import { eachDayOfInterval, endOfMonth, startOfMonth, format } from "date-fns";
+import {
+  startOfMonth,
+  endOfMonth,
+  eachDayOfInterval,
+  format,
+  isSameMonth,
+  startOfWeek,
+  endOfWeek,
+  addMonths,
+  subMonths,
+} from "date-fns";
 
-export function getMonthDays(date: Date) {
+export const getCalendarDays = (date: Date): Date[] => {
   const monthStart = startOfMonth(date);
   const monthEnd = endOfMonth(date);
-  return eachDayOfInterval({ start: monthStart, end: monthEnd });
-}
+  const calendarStart = startOfWeek(monthStart, { weekStartsOn: 1 });
+  const calendarEnd = endOfWeek(monthEnd, { weekStartsOn: 1 });
 
-export const formatDate = (date: Date, formatString: string) =>
-  format(date, formatString);
+  return eachDayOfInterval({ start: calendarStart, end: calendarEnd });
+};
+
+export const getMonthYear = (date: Date): string => {
+  return format(date, "MMMM yyyy");
+};
+
+export const isCurrentMonth = (day: Date, currentDate: Date): boolean => {
+  return isSameMonth(day, currentDate);
+};
+
+export const getNextMonth = (date: Date): Date => {
+  return addMonths(date, 1);
+};
+
+export const getPreviousMonth = (date: Date): Date => {
+  return subMonths(date, 1);
+};
+
+export const formatDate = (date: Date, formatString: string): string => {
+  return format(date, formatString);
+};
