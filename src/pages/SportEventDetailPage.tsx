dbfcda211d26be58c5374a202d48309fb8224c90
fsjavier <javier.fersal@gmail.com>
2024-11-09 12:35:29 +0100

    refactor: Move different views to separate components

--- src/pages/SportEventDetailPage.tsx
+++ src/pages/SportEventDetailPage.tsx
@@ -1,87 +1,27 @@
 import { useParams } from "react-router-dom";
 import { SportEvent } from "../types";
 import { validatedDetailSportEvent } from "../utils/sportDataCheckHelper";
-import { capitalizeFirstLetter } from "../utils/textHelper";
 import { useSportEvents } from "../hooks/useSportEvents";
-import AlertMessage from "../components/ui/AlertMessage";
-import GoBack from "../components/ui/GoBack";
 import ErrorMessage from "../components/ui/ErrorMessage";
 import LoadingMessage from "../components/ui/LoadingMessage";
-import SportEventDetailHeader from "../components/sportEvents/SportEventDetailHeader";
-import SportEventDetailInfo from "../components/sportEvents/SportEventDetailInfo";
-import SportEventDetailTeamInfo from "../components/sportEvents/SportEventDetailTeamInfo";
-import SportEventDetailResult from "../components/sportEvents/SportEventDetailResult";
+import SportEventNotFound from "../components/sportEvents/SportEventNotFound";
+import SportEventMissingData from "../components/sportEvents/SportEventMissingData";
+import SportEventDetailView from "../components/sportEvents/SportEventDetailView";
 
 export default function EventDetailPage() {
   const { id } = useParams();
   const { data: events, isLoading, isError, error } = useSportEvents();
+  const event: SportEvent | undefined = events?.find(
+    (e: SportEvent) => e.id === id
+  );
 
   if (isLoading) return <LoadingMessage />;
   if (isError && error)
-    return (
-      <>
-        <GoBack title="Calendar" />
-        <ErrorMessage header="There was an error" message={error.message} />
-      </>
-    );
-
-  const event = events?.find((e: SportEvent) => e.id === id);
-  const validatedStatus = capitalizeFirstLetter(event?.status);
-
-  if (!event) {
-    return (
-      <>
-        <GoBack title="Calendar" />
-        <AlertMessage
-          header="Event not found."
-          message="The requested event could not be found."
-        />
-      </>
-    );
-  }
+    return <ErrorMessage header="There was an error" message={error.message} />;
+  if (!event) return <SportEventNotFound />;
 
   const isEventValid = validatedDetailSportEvent(event);
-  if (!isEventValid) {
-    return (
-      <>
-        <GoBack title="Calendar" />
-        <AlertMessage
-          header="Event data is missing important information."
-          message="Please try again later."
-        />
-      </>
-    );
-  }
+  if (!isEventValid) return <SportEventMissingData />;
 
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <SportEventDetailHeader
-        homeTeam={event.homeTeam?.name}
-        awayTeam={event.awayTeam?.name}
-        competitionName={event.originCompetitionName}
-      />
-      <div className="bg-white shadow-lg rounded-b-lg overflow-hidden">
-        <div className="p-6">
-          <SportEventDetailInfo
-            date={event.dateVenue}
-            time={event.timeVenueUTC}
-            stage={event.stage?.name}
-            stadium={event.stadium}
-          />
-          <SportEventDetailTeamInfo
-            homeTeam={event.homeTeam}
-            awayTeam={event.awayTeam}
-          />
-          {event.status === "played" && (
-            <SportEventDetailResult result={event.result} />
-          )}
-          <div className="mt-6">
-            <p className="text-sm text-gray-500">
-              <span className="font-semibold">Status:</span> {validatedStatus}
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <SportEventDetailView event={event} />;
 }
