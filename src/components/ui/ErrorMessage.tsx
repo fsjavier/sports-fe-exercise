c35da74af84847df671dd83cbece7559e24c6f56
fsjavier <javier.fersal@gmail.com>
2024-11-09 12:29:44 +0100

    refactor: Add go back to calendar to error message

--- src/components/ui/ErrorMessage.tsx
+++ src/components/ui/ErrorMessage.tsx
@@ -1,3 +1,5 @@
+import GoBackToCalendar from "./GoBackToCalendar";
+
 interface ErrorMessageProps {
   header: string;
   message: string;
@@ -5,12 +7,15 @@ interface ErrorMessageProps {
 
 export default function ErrorMessage({ header, message }: ErrorMessageProps) {
   return (
-    <div
-      className="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded relative"
-      role="alert"
-    >
-      <h2 className="font-bold">{header}</h2>
-      <p>{message}</p>
-    </div>
+    <>
+      {location.pathname !== "/" && <GoBackToCalendar />}
+      <div
+        className="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded relative"
+        role="alert"
+      >
+        <h2 className="font-bold">{header}</h2>
+        <p>{message}</p>
+      </div>
+    </>
   );
 }
--- src/components/ui/GoBackToCalendar.tsx
+++ src/components/ui/GoBackToCalendar.tsx
@@ -1,18 +1,14 @@
 import { ArrowLeft } from "lucide-react";
 import { Link } from "react-router-dom";
 
-interface GoBackProps {
-  title: string;
-}
-
-export default function GoBack({ title }: GoBackProps) {
+export default function GoBackToCalendar() {
   return (
     <Link
       to="/"
       className="inline-flex items-center text-blue-500 hover:text-blue-600 mb-4"
     >
       <ArrowLeft className="mr-2" size={20} />
-      Back to {title}
+      Back to Calendar
     </Link>
   );
 }
