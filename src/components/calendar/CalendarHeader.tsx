8e9fe9f2096f7465ee398a143c19751dfe59eb31
fsjavier <javier.fersal@gmail.com>
2024-11-10 07:47:30 +0100

    feat: Add sport and status filter components

--- src/components/calendar/CalendarHeader.tsx
+++ src/components/calendar/CalendarHeader.tsx
@@ -1,9 +1,24 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import { EventSport, EventStatus } from "../../types";
 import { getMonthYear } from "../../utils/dateHelper";
+import {
+  EVENT_SPORT_OPTIONS,
+  EVENT_STATUS_OPTIONS,
+} from "../../utils/constants";
 import { useCalendar } from "../../context/CalendarContext";
 
+import DropDownFilter from "../ui/DropDownFilter";
+
 export default function CalendarHeader() {
-  const { handlePreviousMonth, handleNextMonth, currentDate } = useCalendar();
+  const {
+    handlePreviousMonth,
+    handleNextMonth,
+    currentDate,
+    sportFilter,
+    setSportFilter,
+    statusFilter,
+    setStatusFilter,
+  } = useCalendar();
 
   return (
     <>
@@ -21,6 +36,20 @@ export default function CalendarHeader() {
         >
           <ChevronRight className="h-6 w-6" />
         </button>
+      </div>
+      <div className="flex flex-col sm:flex-row justify-center gap-4 mb-4">
+        <DropDownFilter
+          label="Status"
+          filter={statusFilter}
+          setFilter={(filter) => setStatusFilter(filter as EventStatus)}
+          filterOptions={EVENT_STATUS_OPTIONS}
+        />
+        <DropDownFilter
+          label="Sport"
+          filter={sportFilter}
+          setFilter={(filter) => setSportFilter(filter as EventSport)}
+          filterOptions={EVENT_SPORT_OPTIONS}
+        />
       </div>
       <div className="grid grid-cols-7 gap-2 mb-2">
         {["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].map((day) => (
